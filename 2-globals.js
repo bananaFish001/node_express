610a482f8377c95d821095a629f5cfc9a68c05ff
bananaFish001 <raj123.rs308@gmail.com>
2024-04-14 19:37:11 +0530

    using require to import a module without export statement

--- 2-globals.js
+++ 2-globals.js
@@ -1,7 +1,9 @@
 const names = require('./4-names')
 const sayHi = require('./5-utils');
+const data = require('./6-alternative')
+console.log(data)
 
-
-sayHi(`duncan`);
-sayHi(names.denji)
-sayHi(names.power)
+require('./7-mind-fuck')
+//sayHi(`duncan`);
+//sayHi(names.denji)
+//sayHi(names.power)
