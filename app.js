92aa48f958abb78cdd187b8c924e1172645d6672
bananaFish001 <raj123.rs308@gmail.com>
2024-04-14 23:49:45 +0530

    some more internal modules of os

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const os = require('os')
-console.log(os.version())
+// info about current user
+const user = os.userInfo()
+console.log(user)
+
+// returns the system uptime
+console.log(`The system uptime is ${os.uptime()}`)
+
+const currentOS = {
+  name:os.type(),
+  release:os.release(),
+  totalMem:os.totalmem(),
+  freeMem:os.freemem()
+}
+
+console.log(currentOS)
