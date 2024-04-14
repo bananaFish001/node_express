38c51717c22b2236943649b82ffb275d5587256b
bananaFish001 <raj123.rs308@gmail.com>
2024-04-14 23:39:21 +0530

    built in modules of node

--- app.js
+++ app.js
@@ -1,9 +1,2 @@
-const names = require('./4-names')
-const sayHi = require('./5-utils');
-const data = require('./6-alternative')
-console.log(data)
-
-require('./7-mind-fuck')
-sayHi(`duncan`);
-sayHi(names.denji)
-sayHi(names.power)
+const os = require('os')
+console.log(os.version())
