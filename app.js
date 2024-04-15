8eb7ce03e4e5593a1a029ac91c1a64a71cbcd578
bananaFish001 <raj123.rs308@gmail.com>
2024-04-15 21:19:12 +0530

    method used in modules

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-const os = require('os')
-// info about current user
-const user = os.userInfo()
-console.log(user)
+const path = require('path')
 
-// returns the system uptime
-console.log(`The system uptime is ${os.uptime()}`)
-
-const currentOS = {
-  name:os.type(),
-  release:os.release(),
-  totalMem:os.totalmem(),
-  freeMem:os.freemem()
-}
-
-console.log(currentOS)
+console.log(path.sep)
