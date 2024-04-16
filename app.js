972e273adb0f38d4321c71e9b9c77bc046598c3c
bananaFish001 <raj123.rs308@gmail.com>
2024-04-16 20:34:20 +0530

    synchronous approach in file sync

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const path = require('path')
+// importing a module from fs
+const { readFileSync, writeFileSync } = require("fs")
 
-console.log(path.sep)
-const filePath = path.join('/New_Folder', 'Subfolder', 'test.txt')
-console.log(filePath)
+const first = readFileSync('./content/first.txt', 'utf-8')
+const second = readFileSync('./content/second.txt', 'utf-8')
 
-const base = path.basename(filePath)
-console.log(base)
+console.log(first, second)
+
+writeFileSync('./content/result-sync.txt', `Here is the result : ${first}, ${second}`, { flag: 'a' })
