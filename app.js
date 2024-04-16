e0f6435d863977cfd6460164df65762f069ab68a
bananaFish001 <raj123.rs308@gmail.com>
2024-04-16 21:13:34 +0530

    removing some errors and running the code

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-const { readFile, writeFile, read, write } = require('fs')
+const { readFile, writeFile } = require('fs');
 
-readFile('./content/first.txt', 'utf8', (err, result) => {
+readFile('./content/first.txt', 'utf8', (err, first) => {
     if (err) {
-        console.log(err)
-        return
+        console.log(err);
+        return;
     }
-    const first = result
 
-    readFile('./content/second.txt', 'utf8', (err, result) => {
+    readFile('./content/second.txt', 'utf8', (err, second) => {
         if (err) {
-            console.log(err)
-            return
+            console.log(err);
+            return;
         }
-        const second = result
 
-        writeFile('./content/result-async.txt', `Here is the result : ${first}, ${second}`,
-            { flag: 'a' }, (err, result) => {
-                if (err) {
-                    console.log(err)
-                    return;
-                }
-                console.log(result)
+        writeFile('./content/result-async.txt', `Here is the result: ${first}, ${second}`, { flag: 'a' }, (err) => {
+            if (err) {
+                console.log(err);
+                return;
             }
-  })
-})
+
+            console.log('Result has been written to result-async.txt');
+        });
+    });
+});
