b8ccb02457d4cf9e27df1eeb355dad7a2586e21b
bananaFish001 <raj123.rs308@gmail.com>
2024-04-17 21:11:48 +0530

    some file tree restructuring

---
+++ 11-fs-async.js
@@ -0,0 +1,24 @@
+const { readFile, writeFile } = require('fs');
+
+readFile('./content/first.txt', 'utf8', (err, first) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    readFile('./content/second.txt', 'utf8', (err, second) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        writeFile('./content/result-async.txt', `Here is the result: ${first}, ${second}`, { flag: 'a' }, (err) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            console.log('Result has been written to result-async.txt');
+        });
+    });
+});
