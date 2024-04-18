dbceb3dbdfa95a2d1ce774992908659f01a5876c
bananaFish001 <raj123.rs308@gmail.com>
2024-04-18 23:11:44 +0530

    just some changes

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,6 +1,6 @@
 // importing a module from fs
 const { readFileSync, writeFileSync } = require("fs")
-
+console.log('start')
 const first = readFileSync('./content/first.txt', 'utf-8')
 const second = readFileSync('./content/second.txt', 'utf-8')
 
@@ -10,3 +10,4 @@ writeFileSync('./content/result-sync.txt',
     `Here is the result : ${first}, ${second}`,
     { flag: 'a' } // flag is added to make sure new content is appended instead of being overwrittern
 )
+console.log('done')
